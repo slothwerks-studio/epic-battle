44d40c3512cd3e3d718348cf01bb6d377910f77d
Sloth I <github@slothwerks.com>
2021-06-21 08:09:26 -0400

    Began working on script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+// Initial values
+const hero = {
+  name: "",
+  strength: 0,
+  power: 0
+};
+
+const villain = {
+  name: "",
+  strength: 0,
+  power: 0
+};
+
+let roundNum = 0;
+
+const introContent = document.getElementById("introContent");
+const battleContent = document.getElementById("battleContent");
+
+// Build a function that will process user input from the build battle form,
+// then initiate the battle
+// This function will be a submission handler and will therefore
+// take an event object as an argument
+function buildBattle(event) {
+  // Prevent default refresh of app
+  event.preventDefault();
+  // Acquire user input from form
+  const heroName = document.getElementById("heroName").value.trim();
+  const heroStrength = document.getElementById("heroStrength").value;
+  const heroPower = document.getElementById("heroPower").value;
+  const villainName = document.getElementById("villainName").value.trim();
+  const villainStrength = document.getElementById("villainStrength").value;
+  const villainPower = document.getElementById("villainPower").value;
+  // Add values to hero and villain objects
+  hero.name = heroName.value.trim();
+  hero.strength = parseInt(heroStrength.value); // Convert to number
+  hero.power = parseInt(heroPower.value); // Convert to number
+  villain.name = villainName.value.trim();
+  villain.strength = parseInt(villainStrength.value); // Convert to number
+  villain.power = parseInt(villainPower.value); // Convert to number
+  // Clear the form
+  heroName.value = "";
+  // Hide intro content and show battle content
+  introContent.classList.add("hidden");
+  battleContent.classList.remove("hidden");
+  // Clear the form
+  buildBattleForm.reset();
+  // TODO: Need to build battle round processor function
+}
+
+// Add handler to form
+const buildBattleForm = document.getElementById("buildBattleForm");
+buildBattleForm.onsubmit = buildBattle;
